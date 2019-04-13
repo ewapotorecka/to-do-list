9707df7add768da9da8dc1d0d442834d9bc93ac4
Ewa Potorecka <ewa.potorecka@gmail.com>
2019-04-13 17:33:50 +0200

    to do list is working!

--- toDoList.js
+++ toDoList.js
@@ -1,35 +1,80 @@
 const addButton = document.getElementById( 'add' );
 const taskInput = document.getElementById( 'task' );
-const taskList = [];
+// tasklist-> tasks
+let taskList = [];
+// funkcja ktora zwraca poczatkowe dane -> przypisanie do tasks
+if ( localStorage.getItem( 'tasklist' ) ) { //'tasklist' powinno byc zmienna
+	//try catch
+	taskList = JSON.parse( localStorage.getItem( 'tasklist' ) );
+	taskList.forEach( createListItem );
+}
 
-addButton.addEventListener( 'click', createListItem );
+taskInput.addEventListener( 'keyup', function( event ) {
+	// key name?
+	if ( event.keyCode === 13 ) {
+		document.getElementById( 'add' ).click();
+	}
+} );
+// pod spodem na osobną funkcję do click i enter
+// taskObject -> task
+addButton.addEventListener( 'click', () => {
+	if ( !taskInput.value ) {
+		return;
+	}
 
-function createListItem() {
-	const task = document.createElement( 'DIV' );
-	task.classList.add( 'task-list-item' );
-	task.innerText = taskInput.value;
-	taskInput.value = '';
-	document.body.appendChild( task );
 	const taskObject = {
-		id: createId(),
-		value: task.innerText,
+		value: taskInput.value,
 		done: false,
 	};
+
 	taskList.push( taskObject );
-	const remove = document.createElement( 'BUTTON' );
-	remove.innerHTML = 'X';
-	remove.classList.add( 'remove' );
-	task.appendChild( remove );
-	task.addEventListener( 'click', event => {
-		if ( event.target === task ) {
-			task.classList.toggle( 'done' );
+	createListItem( taskObject );
+	updateLocalStorage( taskList );
+
+} );
+
+function createListItem( task ) {
+	const taskElement = document.createElement( 'DIV' );
+	const removeButton = document.createElement( 'BUTTON' );
+
+	taskElement.classList.add( 'task-list-item' );
+	taskElement.innerText = task.value;
+	taskInput.value = '';
+	removeButton.innerHTML = 'X';
+	removeButton.classList.add( 'remove' );
+	taskElement.appendChild( removeButton );
+
+	taskElement.classList.toggle( 'done', task.done );
+
+	taskElement.addEventListener( 'click', event => {
+		if ( event.target === taskElement ) {
+			task.done = !task.done;
+			taskElement.classList.toggle( 'done' );
+			updateLocalStorage( taskList );
 		}
 	} );
-	remove.addEventListener( 'click', () => {
-		task.remove();
+
+	removeButton.addEventListener( 'click', () => {
+		const index = taskList.findIndex( t => t == task );
+		taskList.splice( index, 1 );
+		taskElement.remove();
+		updateLocalStorage( taskList );
 	} );
+
+	document.body.appendChild( taskElement );
 }
 
-function createId() {
-	return Math.floor( Math.random() * 100 );
+function updateLocalStorage( taskList ) {
+	localStorage.setItem( 'tasklist', JSON.stringify( taskList ) );
 }
+// function createUniqueId() {
+// 	const id = Math.floor( Math.random() * Math.pow( 10, 6 ) );
+
+// 	for ( const element of taskList ) {
+// 		if ( element.id == id ) {
+// 			return createUniqueId();
+// 		}
+// 	}
+
+// 	return id;
+// }
